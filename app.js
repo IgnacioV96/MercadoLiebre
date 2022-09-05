9e3b8fbb4a0c031f430419790d16eaf280147022
IgnacioV96 <nachovillanueva63@gmail.com>
2022-09-05 00:44:21 -0300

    correccion

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express()
 const publicPath = path.resolve(__dirname, './public')
 
 app.use(express.static(publicPath))
-app.listen(3002, () => console.log("Servidor corriendo"))
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
 
 app.get('/', function(req, res){
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
